61bfd7203080dcd3a03a6bd61a5208308baec351
JBlacklawsTen10 <58850837+JBlacklawsTen10@users.noreply.github.com>
2020-04-28 11:33:59 +0100

    finished tutorial

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,16 +1,64 @@
 package controllers
 
 import javax.inject.Inject
-import play.api.mvc.{BaseController, ControllerComponents}
+import jdk.net.SocketFlow.Status
+import models.DataModel
+import play.api.http.Status
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Result}
+import reactivemongo.core.errors.{DatabaseException, DriverException, GenericDriverException, ReactiveMongoException}
+import repositories.DataRepository
 
-class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
-  def index() = TODO
+import scala.concurrent.{ExecutionContext, Future}
 
-  def create() = TODO
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val dataRepository: DataRepository,
+                                      implicit  val ec: ExecutionContext) extends BaseController{
+  def index(): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.find().map(items => Ok(Json.toJson(items))) recover {
+      case _: ReactiveMongoException => InternalServerError(Json.obj(
+        "message" -> "Error finding item in Mongo"
+      ))
+    }
+  }
 
-  def read(id: String) = TODO
+  def create(): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) =>
+        dataRepository.create(dataModel).map(_ => Created) recover {
+          case _: ReactiveMongoException => InternalServerError(Json.obj(
+            "message" -> "Error creating item in Mongo"
+          ))
+        }
+      case JsError(_) => Future(BadRequest)
+    }
+  }
 
-  def update(id: String) = TODO
+  def read(id: String): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.read(id).map(items => Ok(Json.toJson(items))) recover {
+      case _: ReactiveMongoException => InternalServerError(Json.obj(
+        "message" -> "Error reading item in Mongo"
+      ))
+    }
+  }
 
-  def delete(id: String) = TODO
+  def update(id: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) =>
+        dataRepository.update(dataModel).map(items => Accepted(Json.toJson(items))) recover {
+          case _: ReactiveMongoException => InternalServerError(Json.obj(
+            "message" -> "Error updating item in Mongo"
+          ))
+        }
+      case JsError(_) => Future(BadRequest)
+    }
+  }
+
+
+  def delete(id: String): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.delete(id).map(_ => Status(ACCEPTED)) recover {
+      case _: ReactiveMongoException => InternalServerError(Json.obj(
+        "message" -> "Error deleting item in Mongo"
+      ))
+    }
+  }
 }
